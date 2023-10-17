26ab83c55b9268101b0c0a34228d82656ec5eb97
Donnie Flood <donnie@floodfx.com>
2023-10-16 22:55:59 -0600

    - add hibernate validations lib - test validations

---
+++ src/test/java/com/undead4j/form/MyForm.java
@@ -0,0 +1,33 @@
+package com.undead4j.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class MyForm {
+  @NotBlank()
+  private String name;
+
+
+  @Email()
+  @NotBlank()
+  private String email;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+
+
+}
