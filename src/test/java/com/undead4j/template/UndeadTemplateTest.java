d4729890a2b483dadfc4566befa6c04991fde174
Donnie Flood <donnie@floodfx.com>
2023-10-30 17:55:50 -0600

    tests for templates and directives

--- src/test/java/com/undead4j/template/UndeadTemplateTest.java
+++ src/test/java/com/undead4j/template/UndeadTemplateTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static com.undead4j.template.Undead.*;
+import static com.undead4j.template.Directive.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UndeadTemplateTest {
