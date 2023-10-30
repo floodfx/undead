28eb8d37ce854c5018c1e97558c349eebed12f6f
Donnie Flood <donnie@floodfx.com>
2023-10-30 09:19:14 -0600

    add docs to UndeadEvent and UndeadInfo and their implementations

--- src/main/java/com/undead4j/event/SimpleUndeadEvent.java
+++ src/main/java/com/undead4j/event/SimpleUndeadEvent.java
@@ -2,5 +2,10 @@ package com.undead4j.event;
 
 import com.undead4j.url.Values;
 
+/**
+ * SimpleUndeadEvent is a simple implementation of {@link UndeadEvent} that is used internally by Undead.
+ * @param type the type of the event
+ * @param data the data associated with the event
+ */
 public record SimpleUndeadEvent(String type, Values data) implements UndeadEvent {
 }
--- src/main/java/com/undead4j/event/UndeadInfo.java
+++ src/main/java/com/undead4j/event/UndeadInfo.java
@@ -2,8 +2,23 @@ package com.undead4j.event;
 
 import java.util.Map;
 
+/**
+ * UndeadInfo is the interface for all events that are from the server (either from a {@link com.undead4j.socket.Socket#sendInfo(UndeadInfo)}
+ * or from a pub/sub subscription.  Since this data comes directly from another server side process, Undead
+ * does not parse it or otherwise modify it.  It is up to the developer to ensure that the data is valid and
+ * the key/value pairs are correct.
+ */
 public interface UndeadInfo {
+
+  /**
+   * type is the type of event
+   * @return the type of the event
+   */
   String type();
 
+  /**
+   * data is the data associated with the event
+   * @return a map of key/value pairs
+   */
   Map data();
 }
