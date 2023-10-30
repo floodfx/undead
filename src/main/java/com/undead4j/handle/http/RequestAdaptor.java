628c8c19bbea802b988066f23ee4f6ed5feb51a9
Donnie Flood <donnie@floodfx.com>
2023-10-30 10:16:37 -0600

    require query params for request adaptor and add javadocs

--- src/main/java/com/undead4j/handle/http/RequestAdaptor.java
+++ src/main/java/com/undead4j/handle/http/RequestAdaptor.java
@@ -2,14 +2,47 @@ package com.undead4j.handle.http;
 
 import java.util.Map;
 
+/**
+ * RequestAdaptor is an interface for adapting the HTTP request from a server
+ * framework to the Undead render lifecycle.  This normalizes the HTTP request
+ * so that the Undead render lifecycle can be used with any server framework that
+ * can adapt its request to this interface.
+ */
 public interface RequestAdaptor {
-  Map sessionData();
 
-  String url();
+  /**
+   * The session data from the HTTP request
+   * @return a Map of session data
+   */
+  public Map sessionData();
 
-  String path();
+  /**
+   * The URL of the HTTP request
+   * @return the URL
+   */
+  public String url();
 
-  Map pathParams();
+  /**
+   * The path of the HTTP request
+   * @return the path
+   */
+  public String path();
 
-  void willRedirect(String destURL);
+  /**
+   * The path parameters of the HTTP request
+   * @return a Map of path parameters
+   */
+  public Map pathParams();
+
+  /**
+   * The query parameters of the HTTP request
+   * @return a Map of query parameters
+   */
+  public Map queryParams();
+
+  /**
+   * A callback to deliver redirect URLs to the server framework
+   * @param destURL
+   */
+  public void willRedirect(String destURL);
 }
